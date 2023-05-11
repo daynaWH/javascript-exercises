c352f3c6c7014979cd5468543fb6f3991b22646f
daynaWH <118218031+daynaWH@users.noreply.github.com>
2023-05-11 14:52:01 -0400

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let splitString = string.split('');
+  let reverseString = splitString.reverse('');
+  let resultString = reverseString.join('');
+  return resultString;
 };
 
 // Do not edit below this line
